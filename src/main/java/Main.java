4b5e0496e1a1a3b292c5c747dc3803fcb1b3c978
Arhat161 <Arhat161@users.noreply.github.com>
2022-07-01 17:11:26 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static int sum(int value1, int value2, int value3) {
+        return value1 + value2 + value3;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("¬ведите первое число:");
+        int value1 = scanner.nextInt();
+
+        System.out.println("¬ведите второе число:");
+        int value2 = scanner.nextInt();
+
+        System.out.println("¬ведите третье число:");
+        int value3 = scanner.nextInt();
+
+        int result = sum(value1, value2, value3);
+        System.out.println("—умма чисел = " + result);
+
+    }
+}
